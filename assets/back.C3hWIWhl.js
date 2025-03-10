623b6b79197b033c897a5196145a0323f775f7e6
imLKlaus <2358628219@qq.com>
2025-03-11 02:56:24 +0800

    valaxy博客测试/page/*目录刷新报 HTTP 404 状态码问题...

---
+++ assets/back.C3hWIWhl.js
@@ -0,0 +1,1 @@
+import{u as r}from"./chunks/vue-router.B1oR5mI4.js";function e(){const e=r();return{back:function(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}}}export{e as u};
