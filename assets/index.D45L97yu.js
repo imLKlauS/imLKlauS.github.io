4388b129c8dca562069154ad5225d029e9a6d1b4
imLKlaus <2358628219@qq.com>
2025-03-13 01:30:25 +0800

    valaxy博客整理...

--- assets/index.D45L97yu.js
+++
@@ -1,1 +0,0 @@
-import{C as s}from"./app.ejIEQxa2.js";import"./chunks/dayjs.luRsnflP.js";import"./animation.Dk5apPTb.js";function t(s,t="/assets/none.DLM1AMsD.jpg"){const a=s.target;a.setAttribute("data-src",a.src),a.src=t}function a(t){return s(t,{template:"YYYY-MM-DD HH:mm:ss"})}export{a as f,t as o};
