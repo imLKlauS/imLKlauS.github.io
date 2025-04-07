734418437de9fb6c0c8658787e3c66d8b6e0e355
imLKlaus <2358628219@qq.com>
2025-04-07 15:24:00 +0800

    valaxy框架更新+域名使用...

---
+++ assets/back.BkJlNPSV.js
@@ -0,0 +1,1 @@
+import{u as r}from"./chunks/vue-router.Ccdr9IgI.js";function e(){const e=r();return{back:function(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}}}export{e as u};
