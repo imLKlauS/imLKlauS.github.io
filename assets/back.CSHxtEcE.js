d5d7b96605e54bdb712d40da41e20b5b60a9edc4
imLKlaus <2358628219@qq.com>
2025-04-09 15:22:50 +0800

    valaxy测试域名访问...

---
+++ assets/back.CSHxtEcE.js
@@ -0,0 +1,1 @@
+import{u as r}from"./chunks/vue-router.B9iD66lo.js";function e(){const e=r();return{back:function(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}}}export{e as u};
