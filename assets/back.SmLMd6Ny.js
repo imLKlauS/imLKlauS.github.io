3f1d1ed8d92407d8af87abdbb118228200b805e0
imLKlaus <2358628219@qq.com>
2025-04-10 00:58:39 +0800

    valaxy站点图标修改...

--- assets/back.SmLMd6Ny.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./chunks/vue-router.SIb6Vu7M.js";function e(){const e=r();return{back:function(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}}}export{e as u};
