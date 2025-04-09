1d072f3170c4448eee7da29a6d48b3239b5a791c
imLKlaus <2358628219@qq.com>
2025-04-10 01:10:28 +0800

    valaxy补充RSS内容...

--- assets/back.D9EXYWxX.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./chunks/vue-router.CI9JapQr.js";function e(){const e=r();return{back:function(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}}}export{e as u};
