4534dc5af0b6ec812312c65f9933cf5b3dff39ab
imLKlaus <2358628219@qq.com>
2025-04-16 16:11:16 +0800

    valaxy关闭首页页底图标链接 ...

--- assets/back.Dxn88jLG.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./chunks/vue-router.egQIxMkS.js";function e(){const e=r();return{back:function(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}}}export{e as u};
