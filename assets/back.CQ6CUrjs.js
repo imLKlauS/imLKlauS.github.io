08429678afe2cfececd1ca6d4ee807a3cb57270f
imLKlaus <2358628219@qq.com>
2025-12-04 00:05:43 +0800

    valaxy博客更新 ...

---
+++ assets/back.CQ6CUrjs.js
@@ -0,0 +1,1 @@
+import{u as r}from"./chunks/vue-router.CrE7L6tq.js";function e(){const e=r();return{back:function(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}}}export{e as u};
